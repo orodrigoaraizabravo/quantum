2a61f96daace4ff42adf542f9a1289bc6f7efc30
orodrigoaraizabravo <71989729+orodrigoaraizabravo@users.noreply.github.com>
2022-06-03 17:14:55 -0400

    Update __init__.py

--- tlquantum/__init__.py
+++ tlquantum/__init__.py
@@ -1,7 +1,7 @@
 from .density_tensor import DensityTensor
 from .tt_circuit import TTCircuit, tt_dagger
 from .tt_gates import RotY, RotX, RotZ, Rot, build_binary_gates_unitary, exp_pauli_y, BinaryGatesUnitary, o4_phases, so4, cnot, cz, SO4LR, CNOTL, CNOTR, CZL, CZR, Unitary, IDENTITY, Perceptron_U, Perceptron_WII 
-from .tt_gates import test_function, UnaryGatesUnitary
+from .tt_gates import test_function, UnaryGatesUnitary, PauliString
 from .tt_sum import tt_matrix_sum, tt_sum
 from .maxcut import brute_force_calculate_maxcut, calculate_cut
 from .tt_state import spins_to_tt_state, tt_norm 
